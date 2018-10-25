8a4e8a54ec8c81c320f6bdf250befecc6fe9f16c
Kyle Hassett <kylehassett72@gmail.com>
2018-10-24 20:59:25 -0500

    added cors.rb, updated method

--- runner.rb
+++ runner.rb
@@ -2,24 +2,24 @@ def daily_report
 report = []
 puts "How many hours did you sleep?"
 sleep = gets.chomp.to_i
-  if sleep == (7..9)
-    report << 3
-  elsif sleep == (6..10)
-    report << 2
+  if (7..9).include?(sleep)
+    report << 3.0
+  elsif (6..10).include?(sleep)
+    report << 2.0
   else
-    report << 1
+    report << 1.0
   end
 
   p report
 
 puts "How many calories did you intake today?"
 calories = gets.chomp.to_i
-  if calories = (2000..2500)
-    report << 3
-  elsif calories = (1750..2750)
-    report << 2
+  if (2000..2500).include?(calories)
+    report << 3.0
+  elsif (1750..2750).include?(calories)
+    report << 2.0
   else
-    report << 1
+    report << 1.0
   end
 
   p report
@@ -27,11 +27,11 @@ calories = gets.chomp.to_i
 puts "How many steps did you take today?"
 steps = gets.chomp.to_i.to_i
   if steps >= 10000
-    report << 3
+    report << 3.0
   elsif steps >= 6000
-    report << 2
+    report << 2.0
   else
-    report << 1
+    report << 1.0
   end
 
   p report
@@ -39,16 +39,14 @@ steps = gets.chomp.to_i.to_i
 puts "How many hours of relaxation did you get today?"
 relax = gets.chomp.to_i
   if relax >= 1
-    report << 3
+    report << 3.0
   elsif relax < 1
-    report << 1
+    report << 1.0
   end
 
   p report
 
 (report[0] + report[1] + report[2] + report [3]) / 4
 end
-
-
 
 puts daily_report
