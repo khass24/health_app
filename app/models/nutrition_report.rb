249f96d4304cec1f600cc034e8d4919da57c9023
Kyle Hassett <kylehassett72@gmail.com>
2019-01-29 20:47:53 -0600

    updating Squared Health

--- app/models/nutrition_report.rb
+++ app/models/nutrition_report.rb
@@ -3,7 +3,7 @@ class NutritionReport < DailyReport
 
   def score
     score = 0
-    if count >= 2400 && count <= 2600
+    if count >= 2300 && count <= 2600
       score = 10
     elsif count >= 2200 && count <= 2800
       score = 7.5
